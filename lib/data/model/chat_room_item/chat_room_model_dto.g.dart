41cbb62afa0d37d3dcc254e97eae1d420d805d6c
huannv1 <huan.nv@hesperiafreepress.com>
2022-03-08 16:41:37 +0700

    add logic get data user model

---
+++ lib/data/model/chat_room_item/chat_room_model_dto.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'chat_room_model_dto.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+ChatRoomModelDto _$ChatRoomModelDtoFromJson(Map<String, dynamic> json) =>
+    ChatRoomModelDto(
+      json['id_room'] as String?,
+      json['late_message'] as String?,
+      json['late_time'] as String?,
+      (json['list_user'] as List<dynamic>?)
+          ?.map((e) => UserModelDto.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      json['room_name'] as String?,
+    );
+
+Map<String, dynamic> _$ChatRoomModelDtoToJson(ChatRoomModelDto instance) =>
+    <String, dynamic>{
+      'id_room': instance.id_room,
+      'late_message': instance.late_message,
+      'late_time': instance.late_time,
+      'room_name': instance.room_name,
+      'list_user': instance.list_user,
+    };
